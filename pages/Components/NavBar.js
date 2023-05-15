9c3133503f8a6583f1b6c81a85a4dba4776f9e79
nouran3abdelaal <nourakhaled329@gmail.com>
2023-05-15 12:02:35 +0300

    Initial commit from Create Next App

---
+++ pages/Components/NavBar.js
@@ -0,0 +1,15 @@
+// // import React, { useRouter } from "react";
+
+// const NavBar = () => { //
+//   return (
+//     <nav>
+//       <a href="/">
+//         <i class="fa fa-home"></i> Home
+//       </a>
+//       <a href="/favorites">
+//         <i class="fa fa-heart"></i> Favorites
+//       </a>
+//     </nav>
+//   );
+// };
+// export default  NavBar ;
